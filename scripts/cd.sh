e0be4796442b009dde7da380c9a247cebbf4c473
Kolappan <lekshminarayanan.kolappan>
2021-02-05 08:46:41 +0530

    cd added

--- scripts/cd.sh
+++ scripts/cd.sh
@@ -1,2 +1,8 @@
 #/bin/bash
-/usr/bin/rsync -rlDvcC --progress --chmod=Du+w mywebsite/html/* /var/www/html/
+project=https://github.com/Lforlinux/cicd-example-flow.git
+folder=cicd-example-flow
+
+cd /var/tmp
+cd $folder && git pull
+ls -lrth 
+#/usr/bin/rsync -rlDvcC --progress --chmod=Du+w mywebsite/html/* /var/www/html/
